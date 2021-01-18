bc5e25bb7743a13ec3b1aab7278af86ff5dbd6f6
Swaggerloaf <swaggerloaf@gmail.com>
2021-01-18 14:40:29 -0500

    Create arrays.swift

---
+++ arrays.swift
@@ -0,0 +1,7 @@
+let ingredients: Set = ["cocoa beans", "sugar", "cocoa butter", "salt"]
+
+let othIngredients: Set = ["cocoa beans", "sugar", "cocoa butter", "salt", "pepper"]
+
+if ingredients == othIngredients {
+	print ("you are a copy cat")
+}
